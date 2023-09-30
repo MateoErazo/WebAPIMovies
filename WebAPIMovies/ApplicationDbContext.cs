aaa18a477a81465763720d0da65f9333ca799783
MateoErazo <erazojesusmateo@gmail.com>
2023-09-30 11:12:46 -0500

    Add project files.

---
+++ WebAPIMovies/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPIMovies.Entities;
+
+namespace WebAPIMovies
+{
+  public class ApplicationDbContext : DbContext
+  {
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Gender> Genders { get; set; }
+  }
+}
---
+++ WebAPIMovies/Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPIMovies
+{
+  public class Startup
+  {
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddControllers();
+
+      services.AddDbContext<ApplicationDbContext>(options =>
+      {
+        options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+      });
+
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints => 
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
+}
